f627d5933ef0ef02e27441f0af465e43fb198e59
mondlos <mondloslp@gmail.com>
2017-08-05 11:13:37 +0200

    Create settings.h
    Make it easier to adjust the code to the users needs.

---
+++ the_synth/examples/MIDI_synth/settings.h
@@ -0,0 +1,8 @@
+// Settings for the Arduino Synth library MIDI example
+
+#define SERIAL_BAUDRATE 31250
+
+#define WAVEFORM_CNTRL 13     // Set the Controller numbers here
+#define ENVELOPE_CNTRL 12
+#define LENGTH_CNTRL 10
+#define MODULATION_CNTRL 7
